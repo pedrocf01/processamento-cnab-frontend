bec017c5701d056b215fbbe1007069b1a5357e20
Pedrocf01 <pedrocf01@gmail.com>
2026-05-26 01:12:38 -0300

    Implement retrieval of backend reports

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,28 @@ import {useEffect, useState} from "react"
 
 function App() {
   const [transactions, setTransactions] = useState([]);
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  }
+
+  const uploadFile = async () => {
+    const formData = new FormData();
+    formData.append('file', file);
+    axios.post("http://localhost:8080/cnab/upload", formData, {
+                headers: {'Content-Type': 'multipart/form-data'}
+                });
+  }
 
   const fetchTransactions = async() => {
     const response = await axios.get("http://localhost:8080/transacoes");
     setTransactions(response.data);
+    console.log(response.data);
   }
 
   useEffect(() => {
-    fetchTransactions()
+    fetchTransactions();
   }, []);
 
   return (
@@ -20,27 +34,47 @@ function App() {
       </div>
       <div>
         <span>Choose File</span>
-        <input type="file" accept=".txt"/>
-        <button>Upload File</button>
+        <input type="file" accept=".txt" onChange={handleFileChange} />
+        <button onClick={uploadFile}>Upload File</button>
       </div>
 
       <div>
         <h2>Transações</h2>
         <ul>
-          <li>
-            <table>
-              <thead>
-                <tr>Cartão</tr>
-                <tr>CPF</tr>
-                <tr>Data</tr>
-                <tr>Dono da Loja</tr>
-                <tr>Hora</tr>
-                <tr>Nome da Loja</tr>
-                <tr>Tipo</tr>
-                <tr>Valor</tr>
-              </thead>
-            </table>
-          </li>
+          {transactions.map((report) => (
+            <li key={report.nomeDaLoja}>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Cartão</th>
+                    <th>CPF</th>
+                    <th>Data</th>
+                    <th>Dono da Loja</th>
+                    <th>Hora</th>
+                    <th>Nome da Loja</th>
+                    <th>Tipo</th>
+                    <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                  {report.transacoes.map((transacao) => (
+                    <tr key={transacao.id}>
+                      <td>{transacao.cartao}</td>
+                      <td>{transacao.cpf}</td>
+                      <td>{transacao.data}</td>
+                      <td>{transacao.donoDaLoja}</td>
+                      <td>{transacao.hora}</td>
+                      <td>{transacao.nomeDaLoja}</td>
+                      <td>{transacao.tipo}</td>
+                      <td>{transacao.valor}</td>
+                    </tr>
+                  ))
+                  }
+      
+                </tbody>
+              </table>
+            </li>))
+          }
         </ul>
       </div>
     </div>
